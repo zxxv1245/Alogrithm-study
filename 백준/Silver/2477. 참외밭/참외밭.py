4bf4afd6de6c2218d3c4b9ea3775b1f33bc2b909
zxxv1245 <150820275+zxxv1245@users.noreply.github.com>
2024-03-01 18:49:13 +0900

    [Silver II] Title: 참외밭, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,48 +1,12 @@
 N = int(input())
-maxW = -21e8
-maxH = -21e8
-lst = []
+
+lst = [list(map(int,input().split())) for _ in range(6)]
+
 for _ in range(6) :
-    M, length = map(int,input().split())
-    lst.append((M,length))
-    if M == 1 or M == 2 :
-        if maxW < length :
-            maxW = length
-            maxx = M
-    else :
-        if maxH < length :
-            maxH = length
-            maxy = M
-
-
-
-if (maxx == 2 and maxy == 4)  or (maxx == 1 and maxy == 3) :
-    for i in range(6) :
-        if lst[i][0] == 1 or lst[i][0] == 2 :
-            if lst[i][1] == maxW :
-                if i + 1 < 6 :
-                    orderH = lst[i+1][1]
-                elif i + 1 >= 6 :
-                    orderH = lst[0][1]
-
-        elif lst[i][0] == 3 or lst[i][0] == 4 :
-            if lst[i][1] == maxH :
-                orderW = lst[i - 1][1]
-elif (maxx == 1 and maxy == 4)  or (maxx == 2 and maxy == 3) :
-    for i in range(6):
-        if lst[i][0] == 3 or lst[i][0] == 4:
-            if lst[i][1] == maxH:
-                if i + 1 < 6:
-                    orderW = lst[i + 1][1]
-                elif i + 1 >= 6:
-                    orderW = lst[0][1]
-
-        elif lst[i][0] == 1 or lst[i][0] == 2:
-            if lst[i][1] == maxW:
-                orderH = lst[i - 1][1]
-
-area = (maxW*maxH) - (maxW-orderW)*(maxH-orderH)
-
+    if lst[2][0] == lst[4][0] and lst[3][0] == lst[5][0] :
+        area = lst[0][1]*lst[1][1] - lst[3][1]*lst[4][1]
+    lst.append(lst.pop(0))
+    
 ret = N * area
 
 print(ret)
