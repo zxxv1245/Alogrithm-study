67db7d9d599a86ee3bf6b392cb796d7f65687964
zxxv1245 <150820275+zxxv1245@users.noreply.github.com>
2024-03-28 22:14:52 +0900

    [Silver II] Title: 부분수열의 합, Time: 396 ms, Memory: 113344 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,29 +1,22 @@
-def abc(k,start) :
+def abc(k,arr) :
     global cnt
-    # print(path)
-    if path and sum(path) == S:
-        cnt += 1
+
+
 
 
     if k == N :
-        # if sum(path) == S :
-        #     cnt += 1
+        if len(arr) >= 1 and sum(arr) == S:
+            cnt += 1
         return
 
-    for i in range(start,N) :
-        if v1[i] == 1 : continue
-        v1[i] = 1
-        path.append(lst[i])
-        abc(k+1,i+1)
-        path.pop()
-        v1[i] = 0
+    abc(k+1,arr)
+    abc(k+1,arr+[lst[k]])
+
 
 N,S = map(int,input().split())
 
 lst = list(map(int,input().split()))
-
-path = []
-v1 = [0]*N
+prev = 0
 cnt = 0
-abc(0,0)
+abc(0,[])
 print(cnt)
